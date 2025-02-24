977d77ecce436903944eebc07b00a4d23aa3ea61
piiootr <156233260+piiootr@users.noreply.github.com>
2025-02-24 14:58:43 +0100

    Add files via upload

---
+++ C/0292/main.c
@@ -0,0 +1,20 @@
+/*
+0292
+https://leetcode.com/problems/nim-game/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <stdbool.h>
+
+bool canWinNim(int n) {
+    return (bool)(n % 4);
+}
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
